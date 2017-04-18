e89e51b57b6a196a3e7a5036d3c21789c31c8ab2
Pasquale Kress <wabisabisf@gmail.com>
2017-04-18 15:45:30 +0200

    ...even more stuff

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,20 +13,30 @@ class TasksController < ApplicationController
   end
 
   def create
+    Task.create(task_params)
+    redirect_to tasks_path
   end
 
   def edit
   end
 
   def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description, :done)
   end
 end
