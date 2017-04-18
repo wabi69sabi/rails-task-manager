916b1f9c46f95b4dd44120a38309bb1945449e34
Pasquale Kress <wabisabisf@gmail.com>
2017-04-18 12:31:20 +0200

    ...stuff

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
