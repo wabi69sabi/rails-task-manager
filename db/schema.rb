78f1806254bc9ca691b9d0dc8390decf42210e60
Pasquale Kress <wabisabisf@gmail.com>
2017-04-18 11:58:32 +0200

    added a boolean to task db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418092312) do
+ActiveRecord::Schema.define(version: 20170418093746) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "done",        default: false
   end
 
 end
