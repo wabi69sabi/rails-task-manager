2aff90292ac89aaab70f8292bcf642623137e159
Pasquale Kress <wabisabisf@gmail.com>
2017-04-18 13:28:10 +0200

    ...more stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-
-  get 'tasks/new'
-
-  get 'tasks/show'
-
-  get 'tasks/create'
-
-  get 'tasks/edit'
-
-  get 'tasks/update'
-
-  get 'tasks/destroy'
+  resources :tasks
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
